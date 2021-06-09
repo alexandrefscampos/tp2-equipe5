ec801233f08900c71602639ecb96dfd1a8650945
Alexandre <alexandrefscampos@gmail.com>
2021-06-09 19:41:53 -0300

    resolvendo tudo

--- src/parser/ast.c
+++ src/parser/ast.c
@@ -12,9 +12,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct type *type_create(type_t kind,
-                         struct type *subtype,
-                         struct param_list *params)
+struct type *type_create(type_t kind, struct type *subtype, struct param_list *params)
 {
   struct type *t = malloc(sizeof(*t));
   if (t)
